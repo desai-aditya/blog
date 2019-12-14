ba17695d4eb1ab76dcfcee51584ba2d5517209c2
desai-aditya <desaiadityapr@gmail.com>
2019-12-14 21:08:49 +0530

    Updates

---
+++ precache-manifest.0d54f3b78a792ebb1afcd61dddee0c33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee51591ef1ddb272636120bec79d233",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "27f385592310efc3c7ed",
+    "url": "/blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4c51db2de705ecdfa8dd",
+    "url": "/blog/static/js/2.5bf27056.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/blog/static/js/2.5bf27056.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27f385592310efc3c7ed",
+    "url": "/blog/static/js/main.5d658434.chunk.js"
+  },
+  {
+    "revision": "51fcc8f4ee06cbb51f95",
+    "url": "/blog/static/js/runtime-main.5621d485.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
